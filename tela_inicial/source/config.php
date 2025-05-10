00c708746b397bf18fcabe57c1b26f94af921a6b
Bruno <brunoomendes20@gmail.com>
2025-05-10 16:59:30 -0300

    Remover config.php com credenciais sensíveis

--- tela_inicial/source/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('app_id', 'bfaee680d3a0414baf25a85824c10cfe');
-define('app_secret', 'c64f28ee4fff4d869f5a2446d8ad1ae6');
-define('app_redirect_Uri', 'https://localhost/spotify/tela_inicial/Callback.php');
-
